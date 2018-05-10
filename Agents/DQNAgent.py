b88715779529c68cc012249fef51b4cad30d3c88
xion <cybersquad007@gmail.com>
2018-05-10 23:06:13 +0530

    Added input reshaping to act and remember func

--- Agents/DQNAgent.py
+++ Agents/DQNAgent.py
@@ -51,6 +51,7 @@ class DQNAgent(Agent):
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.num_action)
+        state = state.reshape((1,) + state.shape)
         act_values = self.model.predict(state)
         return np.argmax(act_values[0])
 
@@ -68,16 +69,22 @@ class DQNAgent(Agent):
             self.epsilon *= self.epsilon_decay
 
     def learn(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
+        if batch_size > len(self.memory):
+            batch_size = len(self.memory)
+        mini_batch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in mini_batch:
             target = reward
             if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.model.predict(next_state)[0]))
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+
+    def remember(self, state, action, reward, next_state, done):
+        state = state.reshape((1,) + state.shape)
+        next_state = next_state.reshape((1,) + next_state.shape)
+        super().remember(state, action, reward, next_state, done)
 
 
