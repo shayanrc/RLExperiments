9371d2c2a06cf205d4407cb584d3f42cef4ae534
xion <cybersquad007@gmail.com>
2018-05-16 22:11:42 +0530

    Initial commit of Tensorflow DQN

--- Agents/DQNAgent.py
+++ Agents/DQNAgent.py
@@ -1,7 +1,10 @@
 from .Agent import Agent
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+
+import tensorflow as tf
 
 import numpy as np
 import random
@@ -12,7 +15,8 @@ class DQNAgent(Agent):
     Agent using Deep Q Networks, implemented using keras.
 
     """
-    def __init__(self, state_shape, num_action, memory_size=1000, build_model_func=None, gamma = 0.95,
+
+    def __init__(self, state_shape, num_action, memory_size=1000, build_model_func=None, gamma=0.95,
                  epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995, learning_rate=0.001):
         """
 
@@ -72,6 +76,45 @@ class DQNAgent(Agent):
         self.model.fit(states, targets, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+
+    def remember(self, state, action, reward, next_state, done):
+        state = state.reshape((1,) + state.shape)
+        next_state = next_state.reshape((1,) + next_state.shape)
+        super().remember(state, action, reward, next_state, done)
+
+
+class DQNAgentTF(Agent):
+
+    def __init__(self, state_shape, num_action, memory_size=1000, build_model_func=None, gamma=0.95,
+                 epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.995, learning_rate=0.001):
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.gamma = gamma
+        self.learning_rate = learning_rate
+
+        self.inputs = tf.placeholder('float', shape=[None, state_shape])
+        self.outputs = tf.placeholder('float', shape=[None, num_action])
+
+        if build_model_func is not None and callable(build_model_func):
+            self.__build_model__ = build_model_func
+
+        super().__init__(state_shape, num_action, memory_size=memory_size)
+
+    def __build_model__(self):
+        layer1_weights = tf.Variable(initial_value=tf.random_normal([self.state_shape, 24]))
+        layer1_bias = tf.Variable(initial_value=tf.random_normal([24]))
+        layer1_outputs = tf.nn.relu(tf.add(tf.matmul(self.inputs, layer1_weights), layer1_bias))
+        layer2_weights = tf.Variable(initial_value=tf.random_normal([24, 24]))
+        layer2_bias = tf.Variable(initial_value=tf.random_normal([24]))
+        layer2_outputs = tf.nn.relu(tf.add(tf.matmul(layer1_outputs, layer2_weights), layer2_bias))
+        layer3_weights = tf.Variable(initial_value=tf.random_normal([24, self.num_action]))
+        layer3_bias = tf.Variable(initial_value=tf.random_normal([self.num_action]))
+        model_outputs = tf.nn.relu(tf.add(tf.matmul(layer2_outputs, layer3_weights), layer3_bias))
+        return model_outputs
+
+    def learn(self, batch_size):
+        raise NotImplementedError
 
     def remember(self, state, action, reward, next_state, done):
         state = state.reshape((1,) + state.shape)
