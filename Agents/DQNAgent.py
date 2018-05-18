c0268170c314fdef16a3fa501412b3eb1791b374
xion <cybersquad007@gmail.com>
2018-05-18 09:30:47 +0530

    Fixed __build_model__ and added destructor to close tf session

--- Agents/DQNAgent.py
+++ Agents/DQNAgent.py
@@ -94,14 +94,22 @@ class DQNAgentTF(Agent):
         self.learning_rate = learning_rate
 
         self.inputs = tf.placeholder('float', shape=[None, state_shape])
-        self.outputs = tf.placeholder('float', shape=[None, num_action])
+        self.targets = tf.placeholder('float', shape=[None, num_action])
+
+        self.session = tf.Session()
 
         if build_model_func is not None and callable(build_model_func):
             self.__build_model__ = build_model_func
 
         super().__init__(state_shape, num_action, memory_size=memory_size)
 
+        self.model_action, self.model_outputs, self.optimizer = self.model
+
     def __build_model__(self):
+        """
+        Function to build a tensorflow model
+        :return: A tuple containing the model's predicted action, output and the optimizer used for training the model
+        """
         layer1_weights = tf.Variable(initial_value=tf.random_normal([self.state_shape, 24]))
         layer1_bias = tf.Variable(initial_value=tf.random_normal([24]))
         layer1_outputs = tf.nn.relu(tf.add(tf.matmul(self.inputs, layer1_weights), layer1_bias))
@@ -111,14 +119,36 @@ class DQNAgentTF(Agent):
         layer3_weights = tf.Variable(initial_value=tf.random_normal([24, self.num_action]))
         layer3_bias = tf.Variable(initial_value=tf.random_normal([self.num_action]))
         model_outputs = tf.nn.relu(tf.add(tf.matmul(layer2_outputs, layer3_weights), layer3_bias))
-        return model_outputs
+        model_action = tf.argmax(model_outputs)
+        model_loss = tf.losses.mean_squared_error(self.targets, model_outputs)
+        optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(model_loss)
+        self.session.run(tf.global_variables_initializer)
+        return model_action, model_outputs, optimizer
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.num_action)
+        state = state.reshape((1,) + state.shape)
+        action = self.session.run(self.model_action)
+        return action
 
     def learn(self, batch_size):
-        raise NotImplementedError
+        if batch_size > len(self.memory):
+            batch_size = len(self.memory)
+        mini_batch = random.sample(self.memory, batch_size)
+        states = []
+        targets = []
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
 
     def remember(self, state, action, reward, next_state, done):
         state = state.reshape((1,) + state.shape)
         next_state = next_state.reshape((1,) + next_state.shape)
         super().remember(state, action, reward, next_state, done)
+
+    def __del__(self):
+        self.session.close()
 
 
